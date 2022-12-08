36da4ba369ed53b318167ee73489c1480025000b
Yooshkimchi <joshuakim20012345@gmail.com>
2022-12-07 19:54:43 -0500

    added icon, fixed sizing in order.php

--- catalog.php
+++ catalog.php
@@ -14,6 +14,7 @@
 <html>
     <head>
         <title>Grocer-Ease - Catalog</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="style.css">
         <meta name='viewport' content="width=device-width, initial scale=1">
         <style>
--- order.php
+++ order.php
@@ -4,6 +4,7 @@
 <html>
 <header>
         <title>Grocer-Ease</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <meta name='viewport' content="width=device-width, initial scale=1">
         <link rel="stylesheet" href="style.css">
         
@@ -41,7 +42,7 @@
                 color: #133965;
                 background-color: #fff;
                 }
-                label, div div {
+                label, .cardbox2 div {
                 font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
                 font-size: 20px;
                 }
@@ -69,7 +70,7 @@
                 margin-top: 0%;
                 margin-bottom: 2%;
                 }
-                div div,div label,div input {
+                .cardbox2 div,.cardbox2 label,.cardbox2 input {
                 margin-left: 2%;
                 }
                 hr.order {
